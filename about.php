b5a811a07425c3d4b613f9ba1c886735c9555b0f
PALAK <86872221+Palaksh13@users.noreply.github.com>
2021-07-03 14:16:20 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,217 @@
+        <?php include('allhead.php'); ?>
+        <!-- Page Content -->
+        <div class="container" style="max-width: 1200px;">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h1 class="page-header">About
+        <small>Cloud Classroom</small>
+        </h1>
+        <ol class="breadcrumb">
+        <li><a href=" index">Home</a>
+        </li>
+        <li class="active">About</li>
+     </ol>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row">
+        <div class="col-md-6">
+        <img class="img-responsive" src="images/7fb1f193435815a86c8484f82b9589e1 - Copy.jpg" alt="">
+        </div>
+        <div class="col-md-6">
+        <h2>ABOUT CLOUD CLASSROOMS</h2>
+        <p>The <strong>“Cloud Classrooms”</strong> Website (web based application) is useful for the
+students, faculty, guest whomever likes to learn from web using E-Learn
+(Videos), as well Check result, schedules of assessment and all that task like
+event, news, students can find out list of fresh courses offered by them and
+admission procedure, discussion forum, fee structure etc. without going to
+institute. It provides the facility to the students or guest to have complete
+information about the institute.</p><br>
+<p>
+In this application, student can attend his\her missed classes from e-learn.</p>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Team Members -->
+        <div class="row">
+        <div class="col-lg-12">
+        <h2 class="page-header">Our Alumni</h2>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/253f9ac.jpg" alt="">
+        <div class="caption">
+        <h3>MADHU LATA<br>
+            <small>Teacher</small>
+        </h3>
+        <p>Our DATA COMPRESSION teacher is my favorite teacher..</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/sif10225.225782846266003821356.jpg" alt="">
+        <div class="caption">
+        <h3>ABHISHEK SIR<br>
+            <small>Teacher</small>
+        </h3>
+        <p>CLOUD COMPUTING teacher.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/Lærer-studerende-på-UCN.jpg" alt="">
+        <div class="caption">
+        <h3>SAKSHI MALIK<br>
+            <small>Teacher</small>
+        </h3>
+        <p>COMPILER DESIGN teacher</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/teacher2.jpg" alt="">
+        <div class="caption">
+        <h3>SAPNA SINGH <br>
+            <small>Teacher</small>
+        </h3>
+        <p>ENTERPRENEURSHIP DEVELOPMENT teacher.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/teacher222223.jpg" alt="">
+        <div class="caption">
+        <h3>SONIA ARORA<br>
+            <small>Teacher</small>
+        </h3>
+        <p>SHe is very kind to all the students.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        <div class="col-md-4 text-center">
+        <div class="thumbnail">
+        <img class="img-responsive" src="images/new/20160518144520.jpg" alt="">
+        <div class="caption">
+        <h3>VR.MISHRA<br>
+            <small>Teacher</small>
+        </h3>
+        <p>SHe teaches the lessons every day and keeps tests regularly.</p>
+        <ul class="list-inline">
+            <li><a href="#"><i class="fa fa-2x fa-facebook-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-linkedin-square"></i></a>
+            </li>
+            <li><a href="#"><i class="fa fa-2x fa-twitter-square"></i></a>
+            </li>
+        </ul>
+        </div>
+        </div>
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Our Customers -->
+        <div class="row">
+        <div class="col-lg-12">
+            <h2 class="page-header">Our Student's</h2>
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/welcome46.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/smilingFemaleStudent.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Returning-Student.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/Hopeful_student_1.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/happy_student4.jpg" alt="">
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        <img class="img-responsive customer-img" src="images/library_guy_900x400.jpg" alt="">
+        </div>
+        </div>
+        <!-- /.row -->
+
+        <hr>
+        <!-- Footer -->
+        <footer>
+
+        <p class="footer text-center">Copyright &copy; Cloud Classroom 2018  <strong>MCA</strong> project Made by <strong>VISHAL MATHUR 146891645</strong></p>
+
+        </footer>
+
+        </div>
+        <!-- /.container -->
+        <style>
+
+        .footer{background: #000; padding: 10px 0px; color: #fff;position: fixed;left: 0; right: 0;bottom: -10px;}
+
+        </style>
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Script to Activate the Carousel -->
+        <script>
+        $('.carousel').carousel({
+        interval: 5000 //changes the speed
+        })
+        </script>
+
+        </body>
+        </html>
